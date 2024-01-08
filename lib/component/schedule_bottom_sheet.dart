5468f721416aca3f0d12bd7b8f4f33c8b5dbbf61
hansookim <suekhan@naver.com>
2024-01-09 00:03:02 +0900

    1월9일 집작업

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -1,4 +1,7 @@
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
+
+import 'custom_text_field.dart';
 
 class ScheduleBottomSheet extends StatelessWidget {
   const ScheduleBottomSheet({super.key});
@@ -9,15 +12,102 @@ class ScheduleBottomSheet extends StatelessWidget {
     return Container(
       color: Colors.white,
       height: MediaQuery.of(context).size.height / 2 + bottomInset,
-      child:
-      Padding(
+      child: Padding(
         padding: EdgeInsets.only(bottom: bottomInset),
-        child: Column(
-          children: [
-            TextField(),
-          ],
+        child: Padding(
+          padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Time(),
+              SizedBox(
+                height: 16.0,
+              ),
+              _Content(),
+              SizedBox(height: 16.0),
+              _ColorPicker(),
+              SizedBox(height: 16.0),
+              _SaveButton(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _Time extends StatelessWidget {
+  const _Time({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: CustomTextField(label: '시작시간')),
+        SizedBox(
+          width: 16.0,
+        ),
+        Expanded(child: CustomTextField(label: '마감시간')),
+      ],
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextField(label: '내용');
+  }
+}
+
+class _ColorPicker extends StatelessWidget {
+  const _ColorPicker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 10.0,
+      children: [
+        renderColor(Colors.red),
+        renderColor(Colors.orange),
+        renderColor(Colors.yellow),
+        renderColor(Colors.green),
+        renderColor(Colors.blue),
+        renderColor(Colors.indigo),
+        renderColor(Colors.purple),
+      ],
+    );
+  }
+
+  Widget renderColor(Color color) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+      width: 32.0,
+      height: 32.0,
+    );
+  }
+}
+
+class _SaveButton extends StatelessWidget {
+  const _SaveButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+            child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(backgroundColor: PRIMARY_COLOR),
+          child: Text('저장'),
+        )),
+      ],
     );
   }
 }
