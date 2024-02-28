a4ff212aeb2cc7d890b46b7843efd8624419482d
suekhan <suekhan@naver.com>
2024-02-28 16:06:29 +0900

    2월28일 삼실 작업내용

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -1,5 +1,9 @@
 import 'package:calendar_scheduler/const/colors.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:calendar_scheduler/model/category_color.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
 
 import 'custom_text_field.dart';
 
@@ -16,13 +20,11 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
   int? startTime;
   int? endTime;
   String? content;
+  int? selectedColorId;
 
   @override
   Widget build(BuildContext context) {
-    final bottomInset = MediaQuery
-        .of(context)
-        .viewInsets
-        .bottom;
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).requestFocus(FocusNode());
@@ -30,10 +32,7 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
       child: SafeArea(
         child: Container(
           color: Colors.white,
-          height: MediaQuery
-              .of(context)
-              .size
-              .height / 2 + bottomInset,
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
           child: Padding(
             padding: EdgeInsets.only(bottom: bottomInset),
             child: Padding(
@@ -44,20 +43,41 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    _Time(onStartSaved: (String? val) {
-                      startTime = int.parse(val!);
-                    },
+                    _Time(
+                      onStartSaved: (String? val) {
+                        startTime = int.parse(val!);
+                      },
                       onEndSaved: (String? val) {
                         endTime = int.parse(val!);
-                      },),
+                      },
+                    ),
                     SizedBox(
                       height: 16.0,
                     ),
-                    _Content(onSaved: (String? val) {
-                      content = val!;
-                    },),
+                    _Content(
+                      onSaved: (String? val) {
+                        content = val!;
+                      },
+                    ),
                     SizedBox(height: 16.0),
-                    _ColorPicker(),
+                    FutureBuilder<List<CategoryColor>>(
+                        future: GetIt.I<LocalDatabase>().getCategoryColors(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData &&
+                              selectedColorId == null &&
+                              snapshot.data!.isNotEmpty) {
+                            selectedColorId = snapshot.data![0].id;
+                          }
+                          return _ColorPicker(
+                            colors: snapshot.hasData ? snapshot.data! : [],
+                            selectedColorId: selectedColorId!,
+                            colorIdSetter: (int id) {
+                              setState(() {
+                                selectedColorId = id;
+                              });
+                            },
+                          );
+                        }),
                     SizedBox(height: 16.0),
                     _SaveButton(onPressed: onSavePressed),
                   ],
@@ -107,19 +127,19 @@ class _Time extends StatelessWidget {
       children: [
         Expanded(
             child: CustomTextField(
-              onSaved: onStartSaved,
-              label: '시작시간',
-              isTime: true,
-            )),
+          onSaved: onStartSaved,
+          label: '시작시간',
+          isTime: true,
+        )),
         SizedBox(
           width: 16.0,
         ),
         Expanded(
             child: CustomTextField(
-              onSaved: onEndSaved,
-              label: '마감시간',
-              isTime: true,
-            )),
+          onSaved: onEndSaved,
+          label: '마감시간',
+          isTime: true,
+        )),
       ],
     );
   }
@@ -128,7 +148,10 @@ class _Time extends StatelessWidget {
 class _Content extends StatelessWidget {
   final FormFieldSetter<String> onSaved;
 
-  const _Content({required this.onSaved, super.key,});
+  const _Content({
+    required this.onSaved,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -142,31 +165,55 @@ class _Content extends StatelessWidget {
   }
 }
 
+typedef ColorIdSetter = void Function(int id);
+
 class _ColorPicker extends StatelessWidget {
-  const _ColorPicker({super.key});
+  final List<CategoryColor> colors;
+  final int selectedColorId;
+  final ColorIdSetter colorIdSetter;
+
+  const _ColorPicker({
+    required this.colorIdSetter,
+    required this.colors,
+    required this.selectedColorId,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Wrap(
       spacing: 8.0,
       runSpacing: 10.0,
-      children: [
-        renderColor(Colors.red),
-        renderColor(Colors.orange),
-        renderColor(Colors.yellow),
-        renderColor(Colors.green),
-        renderColor(Colors.blue),
-        renderColor(Colors.indigo),
-        renderColor(Colors.purple),
-      ],
+      children: colors
+          .map((e) => GestureDetector(
+                onTap: () {
+                  colorIdSetter(e.id);
+                },
+                child: renderColor(
+                  e,
+                  selectedColorId == e.id,
+                ),
+              ))
+          .toList(),
     );
   }
 
-  Widget renderColor(Color color) {
+  Widget renderColor(CategoryColor color, bool isSelected) {
     return Container(
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: color,
+        color: Color(
+          int.parse(
+            'FF${color.hexCode}',
+            radix: 16,
+          ),
+        ),
+        border: isSelected
+            ? Border.all(
+                color: Colors.black,
+                width: 4.0,
+              )
+            : null,
       ),
       width: 32.0,
       height: 32.0,
